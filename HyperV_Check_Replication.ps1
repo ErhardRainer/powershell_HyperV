e70b4c9248320d3ebe13e3bf1b442051d7fa465f
ErhardRainer <erhard.rainer@googlemail.com>
2021-12-30 11:35:37 +0100

    Überarbeitung der Skripte
    neu: HyperV_Check_Replication
    geändert: HyperV_restart + Mail

---
+++ HyperV_Check_Replication.ps1
@@ -0,0 +1,37 @@
+﻿<#
+	.NOTES
+	===========================================================================
+	 Created on:   	2021-12-28
+	 Created by:   	Erhard Rainer
+	 Filename:     	HyperV_Check_Replication.ps1
+	===========================================================================
+	.DESCRIPTION
+        Checks Replication Hyper-V
+    .NOTE
+        Change Credentials before Use
+    .VERSION HISTORY
+#>
+
+$count = (Get-VMReplication | Where-Object { $_.Health -ne "Normal"}).Count
+$res = (Measure-VMReplication | Select-Object * | ConvertTo-Html)
+Write-Host $count
+
+if ($count -gt 0)
+{
+    Write-Host "Send Mail because Replication Error"
+    $EmailFrom = "xy@googlemail.com"
+    $EmailTo = "ab@gmail.com"
+    $Subject = "Hyper-V Replication ERROR"
+    $Body = $res
+
+    $mail = New-Object System.Net.Mail.Mailmessage $EmailFrom, $EmailTo, $Subject, $Body
+    $mail.IsBodyHTML=$true
+
+    $SMTPServer = "smtp.gmail.com"
+    $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+    $SMTPClient.EnableSsl = $true
+    $SMTPClient.Credentials = New-Object System.Net.NetworkCredential("username", "pw");
+    $SMTPClient.Send($mail)
+
+
+}
--- HyperV_restart.ps1
+++ HyperV_restart.ps1
@@ -7,6 +7,8 @@
 	===========================================================================
 	.DESCRIPTION
         Dieses Script stellt sicher, dass bestimmte VMs immer laufen
+    .VERSION HISTORY
+        2021-12-23 - ER - Email Versand bei Restart der VM
 #>
 
 ## globale Variable - hier wird konfiguriert, welche VMs laufen müssen
@@ -20,6 +22,17 @@ for ($i=0; $i -lt $VMNames.length; $i++)
     if (($VM | Measure-Object).Count -ne 0) 
     {
         Write-Host "Hyper-V war nicht verfügbar und musste neu gestartet werden" -ForegroundColor Red
+
+        Write-Host "Send Mail because Hyper-V Restarted"
+        $EmailFrom = "xy@googlemail.com"
+        $EmailTo = "ab@gmail.com"
+        $Subject = "TEST"
+        $Body = "this is a notification from XYZ Notifications.."
+        $SMTPServer = "smtp.gmail.com"
+        $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+        $SMTPClient.EnableSsl = $true
+        $SMTPClient.Credentials = New-Object System.Net.NetworkCredential("username", "pw");
+        $SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
 
         Start-VM $VM 
     } else {
