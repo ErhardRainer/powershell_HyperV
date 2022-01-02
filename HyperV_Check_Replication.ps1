e53cd9ba17c29f3a6910e2ecef9afdc0bc670083
ErhardRainer <erhard.rainer@googlemail.com>
2022-01-02 01:32:36 +0100

    Update HyperV_Check_Replication.ps1
    Auslagerung der Settings in XML & Logging

--- HyperV_Check_Replication.ps1
+++ HyperV_Check_Replication.ps1
@@ -10,27 +10,38 @@
     .NOTE
         Change Credentials before Use
     .VERSION HISTORY
+        2022-01-01 ER Settings from XML
+        2020-01-01 ER also logging to Network Share => for Power BI Report
 #>
 
+Write-Host "-------------------------------------------------------"
+Write-Host "             Checking Hyper-V Replications"
+Write-Host "-------------------------------------------------------"
+
+
+[xml]$config = Get-Content "mysettings.xml"
+$timestamp = ($(get-date -f yyyyMMdd)+$(get-date -f HHmmss))
 $count = (Get-VMReplication | Where-Object { $_.Health -ne "Normal"}).Count
 $res = (Measure-VMReplication | Select-Object * | ConvertTo-Html)
-Write-Host $count
+Measure-VMReplication | Select-Object * | Export-Clixml  (-join($config.Settings.ReplicationSettings.LogPath,"replication_",$timestamp,".xml"))
+
+Write-Host "Not Normal Working Replications: $count"
 
 if ($count -gt 0)
 {
     Write-Host "Send Mail because Replication Error"
-    $EmailFrom = "xy@googlemail.com"
-    $EmailTo = "ab@gmail.com"
+    $EmailFrom = $config.Settings.EmailSettings.MailFrom
+    $EmailTo = $config.Settings.EmailSettings.MailTO
     $Subject = "Hyper-V Replication ERROR"
     $Body = $res
 
     $mail = New-Object System.Net.Mail.Mailmessage $EmailFrom, $EmailTo, $Subject, $Body
     $mail.IsBodyHTML=$true
 
-    $SMTPServer = "smtp.gmail.com"
-    $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+    $SMTPServer = $config.Settings.EmailSettings.SMTPServer
+    $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, $config.Settings.EmailSettings.SMTPServer)
     $SMTPClient.EnableSsl = $true
-    $SMTPClient.Credentials = New-Object System.Net.NetworkCredential("username", "pw");
+    $SMTPClient.Credentials = New-Object System.Net.NetworkCredential($config.Settings.EmailSettings.MailUsername, $config.Settings.EmailSettings.MailPassword);
     $SMTPClient.Send($mail)
 
 
