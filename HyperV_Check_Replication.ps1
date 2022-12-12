ec12b44b715771e37afe95dbf4e0f9152b1835af
ErhardRainer <erhard.rainer@googlemail.com>
2022-12-13 00:52:58 +0100

    Update HyperV_Check_Replication.ps1
    Start Resync, if Syncronisation is Critical

--- HyperV_Check_Replication.ps1
+++ HyperV_Check_Replication.ps1
@@ -11,17 +11,18 @@
         Change Credentials before Use
     .VERSION HISTORY
         2022-01-01 ER Settings from XML
-        2020-01-01 ER also logging to Network Share => for Power BI Report
+        2022-01-01 ER also logging to Network Share => for Power BI Report
+        2022-12-13 ER also Start Resyncing
 #>
 
 Write-Host "-------------------------------------------------------"
 Write-Host "             Checking Hyper-V Replications"
 Write-Host "-------------------------------------------------------"
 
-
+$computer = ($env:computername)
 [xml]$config = Get-Content "mysettings.xml"
 $timestamp = ($(get-date -f yyyyMMdd)+$(get-date -f HHmmss))
-$count = (Get-VMReplication | Where-Object { $_.Health -ne "Normal"}).Count
+$count = (Get-VMReplication -ComputerName $computer | Where-Object { $_.Health -ne "Normal"}).Count
 $res = (Measure-VMReplication | Select-Object * | ConvertTo-Html)
 Measure-VMReplication | Select-Object * | Export-Clixml  (-join($config.Settings.ReplicationSettings.LogPath,"replication_",$timestamp,".xml"))
 
@@ -45,4 +46,31 @@ if ($count -gt 0)
     $SMTPClient.Send($mail)
 
 
+}
+
+## Try to Resync Hyper-V that are Critical
+$wait = 5
+
+Get-VMReplication -ComputerName $computer | Format-Table -Property VMName, ReplicationHealth, State
+
+$FailedReplications = Get-VMReplication -ComputerName $computer | Where-Object {$_.ReplicationHealth -eq "Critical"}
+
+$FailedReplications | ForEach-Object {
+$VMName = $_.VMName
+Write-Host $VMName -ForegroundColor Red
+if ($_.State -ne "Resynchronizing")
+    {
+        Write-Host "Replication is resynchronizing $_.VMName"
+        Resume-VMReplication -VMName $VMName -ReplicaServer $computer
+        Start-Sleep -Seconds ($wait * 60)
+        $ReplicationStatus = Get-VMReplication -ComputerName $computer -VMName $_.VMName | Where-Object {$_.State -eq "Resynchronizing"}
+        If ($ReplicationStatus) {
+            Write-Host "Replication $VMName is resynchronizing" -BackgroundColor Green -ForegroundColor Black
+        }
+        Else {
+            Write-Host "Restarting $VMName Replication failed." -BackgroundColor Red -ForegroundColor Black
+        }
+    } else {
+        Write-Host "Replication $VMName already resyncing" -BackgroundColor Yellow -ForegroundColor Black
+    }
 }
